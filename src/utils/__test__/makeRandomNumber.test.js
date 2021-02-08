11061a6563d037d1f072fa8f205cad980d3b16fb
Sergei Kostrikin <cptlbc@yandex.ru>
2021-02-08 18:35:09 -0500

    Refactor test

--- src/utils/__test__/makeRandomNumber.test.js
+++ src/utils/__test__/makeRandomNumber.test.js
@@ -3,6 +3,6 @@ const { makeRandomNumber } = require("../makeRandomNumber");
 test("Random number has to be between passed parameters", () => {
   const min = 0;
   const max = 99;
-  expect(makeRandomNumber(min, max)).toBeGreaterThanOrEqual(0);
-  expect(makeRandomNumber(min, max)).toBeLessThanOrEqual(99);
+  expect(makeRandomNumber(min, max)).toBeGreaterThanOrEqual(min);
+  expect(makeRandomNumber(min, max)).toBeLessThanOrEqual(max);
 });
