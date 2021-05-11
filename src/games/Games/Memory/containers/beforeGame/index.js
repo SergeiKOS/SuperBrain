a14f43251c2081b6c12888fb3ac2584057ac92ef
Serge <s.a.kostrikin@gmail.com>
2021-05-11 11:06:29 -0400

    Add button animation in Memory game

--- src/games/Games/Memory/containers/beforeGame/index.js
+++ src/games/Games/Memory/containers/beforeGame/index.js
@@ -9,7 +9,13 @@ const BeforeGame = () => {
         In this game you have to remember shown squares and after they disapear
         click on them. There are 12 levels. Click for start when ready.
       </Text>
-      <Link to="/games/memory/game">Start</Link>
+      <Link
+        to="/games/memory/game"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+      >
+        Start
+      </Link>
     </BeforeGameWrapper>
   );
 };
--- src/games/Games/Memory/containers/beforeGame/style/beforeGame.js
+++ src/games/Games/Memory/containers/beforeGame/style/beforeGame.js
@@ -1,34 +1,36 @@
 import styled from "styled-components";
 
 import { Link as RouterLink } from "react-router-dom";
-import {colors} from '../../../../../../commonStyles/variables'
+import { motion } from "framer-motion";
+
+import { colors } from "../../../../../../commonStyles/variables";
 
 export const BeforeGameWrapper = styled.div`
   width: 300px;
   margin-left: auto;
   margin-right: auto;
-`
+`;
 
 export const Text = styled.p`
-margin-top: 20px;
-margin-bottom: 20px;
-position: relative;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  position: relative;
 
-&:before {
-  content: '';
-  width: 3px;
-  height: 100%;
-  background-color: ${colors.primary};
-  position: absolute;
-  top: 0;
-  left: -20px;
-}
-`
+  &:before {
+    content: "";
+    width: 3px;
+    height: 100%;
+    background-color: ${colors.primary};
+    position: absolute;
+    top: 0;
+    left: -20px;
+  }
+`;
 
-export const Link = styled(RouterLink)`
+export const Link = styled(motion(RouterLink))`
   display: block;
   text-align: center;
   padding-top: 10px;
   padding-bottom: 10px;
-  border: 1px solid ${colors.primary};;
+  border: 1px solid ${colors.primary}; ;
 `;
